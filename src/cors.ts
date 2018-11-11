0a99a923633be9d34669eec82aaad188b0e7db24
Jeremy Danyow <jdanyow@gmail.com>
2018-11-10 23:03:30 -0800

    switch to cloudflare worker

--- src/cors.ts
+++ src/cors.ts
@@ -1,10 +1,8 @@
-import { ServerResponse } from 'http';
-
-export function addCorsHeaders(res: ServerResponse, origins: string[], requestOrigin: string | undefined) {
-  const permittedOrigin = requestOrigin === undefined || origins.indexOf(requestOrigin) === -1 ? origins[0] : requestOrigin;
-  res.setHeader('Access-Control-Allow-Origin', permittedOrigin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
+export function addCorsHeaders(res: Response, origins: string[], requestOrigin: string | null) {
+  const permittedOrigin = requestOrigin === null || origins.indexOf(requestOrigin) === -1 ? origins[0] : requestOrigin;
+  res.headers.append('Access-Control-Allow-Origin', permittedOrigin);
+  res.headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.headers.append('Access-Control-Allow-Credentials', 'true');
+  res.headers.append('Access-Control-Max-Age', '86400'); // 24 hours
+  res.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
 }
